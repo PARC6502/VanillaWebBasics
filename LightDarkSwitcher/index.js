e7596a13e1b4ecba17f7afba72299aaf5abcbc52
Ibby <theibbster@protonmail.com>
2022-05-17 09:42:53 +0100

    Make code more concise, add more dark mode styling

--- LightDarkSwitcher/index.js
+++ LightDarkSwitcher/index.js
@@ -1,20 +1,19 @@
-let darkMode = false
 let toggle = document.getElementById("toggle")
 let icon = document.getElementById("icon")
 let body = document.body
+let darkMode = body.classList.contains("dark")
 
 toggle.onclick = function toggle() {
+    body.classList.toggle("dark")
     darkMode ? switchToLight() : switchToDark()
 }
 
 function switchToDark() {
     icon.className = "fa-solid fa-sun"
-    body.className = "dark"
     darkMode = true
 }
 
 function switchToLight() {
     icon.className = "fa-solid fa-moon"
-    body.className = "light"
     darkMode = false
 }
