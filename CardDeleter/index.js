36403ee2c381dca7a68c0bad2888ac365d7bb726
Ibby <theibbster@protonmail.com>
2022-05-18 22:04:19 +0100

    Added card creation form, programmatically create initial cards

--- CardDeleter/index.js
+++ CardDeleter/index.js
@@ -1,11 +1,27 @@
-let deleteButtons = document.getElementsByClassName("deletebutton")
+let template = document.querySelector('template').content.firstElementChild
+let wrapper = document.getElementById('wrapper')
 
-for (let deleteButton of deleteButtons) {
-    deleteButton.onclick = function() {
-        let card = deleteButton
-        while (card.className != "card") {
-            card = card.parentElement
-        }
-        card.remove()
-    }
+function cardDeleter(card) {
+    card.remove()
 }
+
+function createCard(cardTitle) {
+    let newCard = template.cloneNode(true)
+    newCard.querySelector('h3').append(cardTitle)
+    newCard.querySelector('.deletebutton').onclick = () => {cardDeleter(newCard)}
+    wrapper.append(newCard)
+}
+
+for (let index = 1; index < 4; index++) {
+    createCard("Card " + index)
+}
+
+let cardCreator = document.getElementById('card-creator')
+
+cardCreator.addEventListener("submit", function(event){
+    event.preventDefault()
+    let titleInput = cardCreator.querySelector('input')
+    let cardTitle = document.createTextNode(titleInput.value || "Card")
+    titleInput.value = ""
+    createCard(cardTitle)
+})
